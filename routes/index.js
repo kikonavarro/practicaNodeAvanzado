cb27e88a3d1b0c05c65f8505e9075d15c7e7773b
kikonavarro <78880702+kikonavarro@users.noreply.github.com>
2021-09-17 22:01:59 +0200

    empezando con EJS

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
+const Anuncio = require("../models/Anuncio");
+const utils = require ("../lib/utils")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get("/", async (req, res, next) => {
+	try {
+		const nombre = req.query.nombre;
+		const venta = req.query.venta;
+		const tags = req.query.tags;
+		const precio = req.query.precio;
+		const skip = parseInt(req.query.skip);
+		const limit = parseInt(req.query.limit);
+		const select = req.query.select;
+
+		const filtro = {};
+
+		if (nombre) {
+			filtro.nombre = nombre;
+		} else if (venta) {
+			filtro.venta = venta;
+		} else if (tags) {
+			filtro.tags = tags;
+		} else if (precio) {
+            let rango = precio.split('-')
+            console.log(rango)
+			if (rango[0] !== '' & rango [1] !== '') {
+				filtro.precio = { $gte: rango[0], $lte: rango[1] };
+			} else if (rango[0] !== '' & rango [1] === '')  {
+				filtro.precio = { $gte: rango[0] };
+			} else if (rango[0] === '' & rango [1] !== '')  {
+				filtro.precio = { $lte: rango[1] };
+			}
+		}
+		const anuncios = await Anuncio.lista(filtro, skip, limit, select);
+    res.render('index', { title: 'Nodepop', anuncios});
+	} catch (err) {
+		next(err);
+    return;
+	}
 });
+
 
 module.exports = router;
